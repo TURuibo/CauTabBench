b06e497ee0125693f6270dc5cf5a99233368d835
TURuibo <ruibo.tu@gmail.com>
2024-09-26 12:53:54 +0200

    add eva dsep table

--- card_gt/eva_causal_table_dsep.py
+++ card_gt/eva_causal_table_dsep.py
@@ -3,29 +3,14 @@ import os,sys
 cwd = os.path.abspath(os.path.curdir)
 sys.path.append(cwd)  # workplace
 
-import pandas as pd
 import numpy as np
 
-from utils.utils import get_args
+from src.utils import get_args
 from sklearn.metrics import RocCurveDisplay
 import matplotlib.pyplot as plt
-def save_results(dataname,seed_sim,response_ls,response_adj_ls,result_path,prefix,questions):
-    with open(cwd+f'{result_path}/{llm}/{prefix}_dsep_response_i1_{dataname}{seed_sim}.txt', 'a') as file:
-        for response,question in zip(response_ls,questions):
-            file.write(f'Question is {question}\n Answer is {response}\n')
-    
-    with open(cwd+f'{result_path}/{llm}/{prefix}_dsep_response_i2_{dataname}{seed_sim}.txt', 'a') as file:
-        for response_adj,question in zip(response_adj_ls,questions):
-            file.write(f'{response_adj}\n')        
-
-def create_output_fiels(dataname,seed_sim,result_path,prefix):
-    with open(cwd+f'{result_path}/{llm}/{prefix}_dsep_response_i1_{dataname}{seed_sim}.txt', 'w') as file:
-        file.write('')
-    with open(cwd+f'{result_path}/{llm}/{prefix}_dsep_response_i2_{dataname}{seed_sim}.txt', 'w') as file:
-        file.write('')
 
 def load_gt_answer(graph_id,prefix):
-    with open(cwd+f'/card_gt/data/{prefix}/{graph_id}_answers.txt', 'r') as file:
+    with open(cwd+f'/data/{prefix}/{graph_id}_answers.txt', 'r') as file:
         answers = file.readlines()
     ans = []
     
@@ -37,26 +22,18 @@ def load_gt_answer(graph_id,prefix):
     return ans
 
 def load_llm_answer(graph_id,result_path,llm,prefix):
-    with open(cwd+f'/card_gt{result_path}/{llm}/{prefix}_dsep_response_lu{graph_id}.txt', 'r') as file:
+    with open(cwd+f'{result_path}/{llm}/{prefix}_dsep_response_lu{graph_id}.txt', 'r') as file:
         answers = file.readlines()
-    
-    with open(cwd+f'/card_gt{result_path}/{llm}/{prefix}_dsep_response_lu{graph_id}_preds.txt', 'w') as file:
-        print(cwd+f'/card_gt{result_path}/{llm}/{prefix}_dsep_response_lu{graph_id}_preds.txt')
-        file.write('')
 
     ans = []
-    with open(cwd+f'/card_gt/{result_path}/{llm}/{prefix}_dsep_response_i2_lu{graph_id}_preds.txt', 'a') as file:
-        for i in range(len(answers)):
-            if answers[i].lower().find('yes') != -1:
-                ans.append(1)
-                file.write('1\n')
-            elif answers[i].lower().find('no') != -1:
-                ans.append(0)
-                file.write('0\n')
-            else:
-                rand_num = np.random.randint(2, size=1)[0]
-                ans.append(rand_num)
-                file.write(f'{rand_num}\n')
+    for i in range(len(answers)):
+        if answers[i].lower().find('yes') != -1:
+            ans.append(1)
+        elif answers[i].lower().find('no') != -1:
+            ans.append(0)
+        else:
+            rand_num = np.random.randint(2, size=1)[0]
+            ans.append(rand_num)
     return ans
 
 if __name__ == "__main__":
@@ -83,5 +60,5 @@ if __name__ == "__main__":
     roc = RocCurveDisplay.from_predictions(labels, preds, name=llm,ax=ax_roc)
     auc = roc.roc_auc
     plt.legend()
-    plt.savefig(cwd+f'/card_gt/result/{llm}/{dataname}.pdf')
+    plt.savefig(cwd+f'/result/{llm}/{dataname}.pdf')
     print(f'{auc:.3f}')
